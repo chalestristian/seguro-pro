ebea27fb6bd1d5302722880309fa486470264271
chalestristian <thales.cristianeugenio@gmail.com>
2025-08-24 21:23:45 -0300

    Configurando DependencyInjection

---
+++ src/PropostaService/PropostaService.CrossCutting.IoC/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PropostaService.Application.Features.Propostas.CriarProposta;
+using PropostaService.Domain.Interfaces;
+using PropostaService.Infrastructure;
+using PropostaService.Infrastructure.Repository;
+
+namespace PropostaService.CrossCutting.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<PropostaDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        services.AddScoped<IPropostaRepository, PropostaRepository>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CriarPropostaCommand).Assembly));
+        services.AddValidatorsFromAssembly(typeof(CriarPropostaCommand).Assembly);
+        
+        return services;
+    }
+}
